e893018b2331caa20acb4da3e56691ffb5a824aa
SWATI SHARAN <158809194+SWATI-SHARAN@users.noreply.github.com>
2026-07-05 19:30:15 +0530

    Set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/MindSync-Appraisal-System/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
